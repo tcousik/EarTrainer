b344474568c6b4b9954ec921cd61a856798f3224
Tejas Cousik <tcousik0@gmail.com>
2020-05-28 21:01:11 -0600

    added all interval links

--- src/QuestionBank/index.js
+++ src/QuestionBank/index.js
@@ -3,14 +3,14 @@ import shortid from "shortid";
 const questionBank = [
   {
     question: "Identify the interval",
-    soundURL: "https://www.dropbox.com/s/9e6rh4nysda81pp/Major%202nd.mp3?dl=0",
+    soundURL: "https://www.dropbox.com/s/bwjsbaaus2jy3vy/Unison.mp3?dl=0",
     choices: ["Unison", "Minor 2nd", "Major 2nd", "Minor 3rd"],
     correct: "Unison",
     questionId: shortid.generate(),
   },
   {
     question: "Identify the interval",
-    soundURL: "https://www.dropbox.com/s/9e6rh4nysda81pp/Major%202nd.mp3?dl=0",
+    soundURL: "https://www.dropbox.com/s/z7ty1a76a8fqvjd/Minor%202nd.mp3?dl=0",
     choices: ["Unison", "Minor 2nd", "Major 2nd", "Minor 3rd"],
     correct: "Minor 2nd",
     questionId: shortid.generate(),
@@ -24,7 +24,7 @@ const questionBank = [
   },
   {
     question: "Identify the interval",
-    soundURL: "https://www.dropbox.com/s/9e6rh4nysda81pp/Major%202nd.mp3?dl=0",
+    soundURL: "https://www.dropbox.com/s/bmske9bnnyqbbby/Minor%203rd.mp3?dl=0",
     choices: ["Unison", "Minor 2nd", "Major 2nd", "Minor 3rd"],
     correct: "Minor 3rd",
     questionId: shortid.generate(),
@@ -32,28 +32,30 @@ const questionBank = [
 
   {
     question: "Identify the interval",
-    soundURL: "https://www.dropbox.com/s/9e6rh4nysda81pp/Major%202nd.mp3?dl=0",
+    soundURL: "https://www.dropbox.com/s/6109vm1cre62toq/Major%203rd.mp3?dl=0",
     choices: ["Major 3rd", "Perfect 4th", "Tritone", "Perfect 5th"],
     correct: "Major 3rd",
     questionId: shortid.generate(),
   },
   {
     question: "Identify the interval",
-    soundURL: "https://www.dropbox.com/s/6109vm1cre62toq/Major%203rd.mp3?dl=0",
+    soundURL:
+      "https://www.dropbox.com/s/0q7qdf8zj7wm657/Perfect%204th.mp3?dl=0",
     choices: ["Major 3rd", "Perfect 4th", "Tritone", "Perfect 5th"],
     correct: "Perfect 4th",
     questionId: shortid.generate(),
   },
   {
     question: "Identify the interval",
-    soundURL: "",
+    soundURL: "https://www.dropbox.com/s/m60m7pcso9n6ebb/Tritone.mp3?dl=0",
     choices: ["Major 3rd", "Perfect 4th", "Tritone", "Perfect 5th"],
     correct: "Tritone",
     questionId: shortid.generate(),
   },
   {
     question: "Identify the interval",
-    soundURL: "",
+    soundURL:
+      "https://www.dropbox.com/s/01rihqvsb5xg29j/Perfect%205th.mp3?dl=0",
     choices: ["Major 3rd", "Perfect 4th", "Tritone", "Perfect 5th"],
     correct: "Perfect 5th",
     questionId: shortid.generate(),
@@ -61,35 +63,35 @@ const questionBank = [
 
   {
     question: "Identify the interval",
-    soundURL: "",
+    soundURL: "https://www.dropbox.com/s/xz9ux35fxnlrx3n/Minor%206th.mp3?dl=0",
     choices: ["Minor 6th", "Major 6th", "Minor 7th", "Major 7th", "Octave"],
     correct: "Minor 6th",
     questionId: shortid.generate(),
   },
   {
     question: "Identify the interval",
-    soundURL: "",
+    soundURL: "https://www.dropbox.com/s/dc9flldkh7aggxi/Major%206th.mp3?dl=0",
     choices: ["Minor 6th", "Major 6th", "Minor 7th", "Major 7th", "Octave"],
     correct: "Major 6th",
     questionId: shortid.generate(),
   },
   {
     question: "Identify the interval",
-    soundURL: "",
+    soundURL: "https://www.dropbox.com/s/m3a2fpz8f3a2qt3/Minor%207th.mp3?dl=0",
     choices: ["Minor 6th", "Major 6th", "Minor 7th", "Major 7th", "Octave"],
     correct: "Minor 7th",
     questionId: shortid.generate(),
   },
   {
     question: "Identify the interval",
-    soundURL: "",
+    soundURL: "https://www.dropbox.com/s/1izj4x1bqch3k3x/Major%207th.mp3?dl=0",
     choices: ["Minor 6th", "Major 6th", "Minor 7th", "Major 7th", "Octave"],
     correct: "Major 7th",
     questionId: shortid.generate(),
   },
   {
     question: "Identify the interval",
-    soundURL: "",
+    soundURL: "https://www.dropbox.com/s/4fphkj649cbiw3k/Octave.mp3?dl=0",
     choices: ["Minor 6th", "Major 6th", "Minor 7th", "Major 7th", "Octave"],
     correct: "Octave",
     questionId: shortid.generate(),
