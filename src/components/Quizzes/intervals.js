072ced4715c23b03f5ef226cd598325e5430c594
Tejas Cousik <tcousik0@gmail.com>
2020-06-03 23:27:36 -0600

    organizing

---
+++ src/components/Quizzes/intervals.js
@@ -0,0 +1,68 @@
+import React from "react";
+import intervalBank from "../Banks/intervalBank";
+import QuestionBox from "../questionBox";
+import Result from "../result";
+import "../../index.css";
+
+export default class App extends React.Component {
+  state = {
+    questions: [],
+    score: 0,
+    responses: 0,
+  };
+
+  fetchIntervals = () => {
+    intervalBank().then((question) => {
+      this.setState({
+        questions: question,
+      });
+    });
+  };
+
+  evaluate = (response, correct) => {
+    if (response === correct) {
+      this.setState({
+        score: this.state.score + 1,
+      });
+    }
+    this.setState({
+      responses: this.state.responses < 4 ? this.state.responses + 1 : 4,
+    });
+  };
+
+  tryAgain = () => {
+    this.fetchIntervals();
+    this.setState({
+      score: 0,
+      responses: 0,
+    });
+  };
+
+  componentDidMount() {
+    this.fetchIntervals();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="title">Ear Trainer</div>
+        {this.state.responses < 4 &&
+          this.state.questions.map(
+            ({ question, soundURL, choices, correct, questionId }) => (
+              <QuestionBox
+                question={question}
+                soundURL={soundURL}
+                choices={choices}
+                key={questionId}
+                chosen={(answer) => this.evaluate(answer, correct)}
+              />
+            )
+          )}
+
+        {this.state.responses === 4 ? (
+          <Result score={this.state.score} tryAgain={this.tryAgain} />
+        ) : null}
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,69 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import intervalBank from "./components/intervalBank";
-import QuestionBox from "./components/questionBox";
-import Result from "./components/result";
+import Intervals from "./components/Quizzes/intervals";
 import "./index.css";
 
 export default class App extends React.Component {
   state = {
-    questions: [],
-    score: 0,
-    responses: 0,
+    displayedComponent: "Home",
   };
-
-  fetchQuestions = () => {
-    intervalBank().then((question) => {
-      this.setState({
-        questions: question,
-      });
-    });
-  };
-
-  evaluate = (response, correct) => {
-    if (response === correct) {
-      this.setState({
-        score: this.state.score + 1,
-      });
-    }
-    this.setState({
-      responses: this.state.responses < 4 ? this.state.responses + 1 : 4,
-    });
-  };
-
-  tryAgain = () => {
-    this.fetchQuestions();
-    this.setState({
-      score: 0,
-      responses: 0,
-    });
-  };
-
-  componentDidMount() {
-    this.fetchQuestions();
-  }
-
   render() {
     return (
-      <div className="container">
-        <div className="title">Ear Trainer</div>
-        {this.state.responses < 4 &&
-          this.state.questions.map(
-            ({ question, soundURL, choices, correct, questionId }) => (
-              <QuestionBox
-                question={question}
-                soundURL={soundURL}
-                choices={choices}
-                key={questionId}
-                chosen={(answer) => this.evaluate(answer, correct)}
-              />
-            )
-          )}
-
-        {this.state.responses === 4 ? (
-          <Result score={this.state.score} tryAgain={this.tryAgain} />
-        ) : null}
+      <div>
+        HEY
+        <Router>
+          <div>
+            <Link to="/intervals">
+              <button>INTS</button>
+            </Link>
+          </div>
+          <Switch>
+            <Route path="/intervals">
+              <Intervals />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
