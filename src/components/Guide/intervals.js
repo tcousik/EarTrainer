9b5b023424ee033d74daf3186bf0682aced6db11
Tejas Cousik <tcousik0@gmail.com>
2020-06-13 00:09:57 -0600

    added int guide

--- src/components/Guide/intervals.js
+++ src/components/Guide/intervals.js
@@ -1,4 +1,40 @@
 import React from "react";
+import "./guide.css";
+import UnisonPic from "../../assets/Intervals/unison.png";
+import UnisonSound from "../../assets/Intervals/Unison.mp3";
+import Minor2ndPic from "../../assets/Intervals/minor2nd.png";
+import Minor2ndSound from "../../assets/Intervals/Minor2nd.mp3";
+import Major2ndPic from "../../assets/Intervals/major2nd.png";
+import Major2ndSound from "../../assets/Intervals/Major2nd.mp3";
+import Minor3rdPic from "../../assets/Intervals/minor3rd.png";
+import Minor3rdSound from "../../assets/Intervals/Minor3rd.mp3";
+import Major3rdPic from "../../assets/Intervals/major3rd.png";
+import Major3rdSound from "../../assets/Intervals/Major3rd.mp3";
+import Perfect4thPic from "../../assets/Intervals/perfect4th.png";
+import Perfect4thSound from "../../assets/Intervals/Perfect4th.mp3";
+import TritonePic from "../../assets/Intervals/tritone.png";
+import TritoneSound from "../../assets/Intervals/Tritone.mp3";
+import Perfect5thPic from "../../assets/Intervals/perfect5th.png";
+import Perfect5thSound from "../../assets/Intervals/Perfect5th.mp3";
+import Minor6thPic from "../../assets/Intervals/minor6th.png";
+import Minor6thSound from "../../assets/Intervals/Minor6th.mp3";
+import Major6thPic from "../../assets/Intervals/major6th.png";
+import Major6thSound from "../../assets/Intervals/Major6th.mp3";
+import Minor7thPic from "../../assets/Intervals/minor7th.png";
+import Minor7thSound from "../../assets/Intervals/Minor7th.mp3";
+import Major7thPic from "../../assets/Intervals/major7th.png";
+import Major7thSound from "../../assets/Intervals/Major7th.mp3";
+import OctavePic from "../../assets/Intervals/octave.png";
+import OctaveSound from "../../assets/Intervals/Octave.mp3";
+import Minor9thPic from "../../assets/Intervals/minor9th.png";
+import Minor9thSound from "../../assets/Intervals/Minor9th.mp3";
+import Major9thPic from "../../assets/Intervals/major9th.png";
+import Major9thSound from "../../assets/Intervals/Major9th.mp3";
+
+const playSound = (src) => {
+  let sound = new Audio(src);
+  sound.play();
+};
 
 export default function IntervalGuide() {
   return (
@@ -20,7 +56,230 @@ export default function IntervalGuide() {
         This section will introduce all the intervals with audio examples.
       </h3>
 
-      <div></div>
+      <div className="title">
+        INTERVALS
+        <table>
+          <thead>
+            <tr>
+              <th>NAME</th>
+              <th>PIC</th>
+              <th>SOUND</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Unison</td>
+              <td>
+                <img alt="" src={UnisonPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(UnisonSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Minor 2nd</td>
+              <td>
+                <img alt="" src={Minor2ndPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Minor2ndSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Major 2nd</td>
+              <td>
+                <img alt="" src={Major2ndPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Major2ndSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Minor 3rd</td>
+              <td>
+                <img alt="" src={Minor3rdPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Minor3rdSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Major 3rd</td>
+              <td>
+                <img alt="" src={Major3rdPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Major3rdSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Perfect 4th</td>
+              <td>
+                <img alt="" src={Perfect4thPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Perfect4thSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Tritone</td>
+              <td>
+                <img alt="" src={TritonePic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(TritoneSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Perfect 5th</td>
+              <td>
+                <img alt="" src={Perfect5thPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Perfect5thSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Minor 6th</td>
+              <td>
+                <img alt="" src={Minor6thPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Minor6thSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Major 6th</td>
+              <td>
+                <img alt="" src={Major6thPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Major6thSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Minor 7th</td>
+              <td>
+                <img alt="" src={Minor7thPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Minor7thSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Major 7th</td>
+              <td>
+                <img alt="" src={Major7thPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Major7thSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Octave</td>
+              <td>
+                <img alt="" src={OctavePic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(OctaveSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Minor 9th</td>
+              <td>
+                <img alt="" src={Minor9thPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Minor9thSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>Major 9th</td>
+              <td>
+                <img alt="" src={Major9thPic} />
+              </td>
+              <td>
+                <button
+                  className="audioBtn"
+                  onClick={() => playSound(Major9thSound)}
+                >
+                  <span>Play</span>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- src/components/Result.js
+++ src/components/Result.js
@@ -3,7 +3,7 @@ import React from "react";
 const Result = ({ score, tryAgain }) => (
   <div className="score-board">
     <div className="score">You answered {score} / 5 questions correctly.</div>
-    <button className="playBtn" onClick={tryAgain}>
+    <button className="replayBtn" onClick={tryAgain}>
       Try again
     </button>
   </div>
