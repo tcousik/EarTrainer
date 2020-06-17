8bb79a9b2e083e18cab2a78f7b91888c9041d2f1
Tejas Cousik <tcousik0@gmail.com>
2020-06-17 15:56:36 -0600

    styled scale guide

--- src/components/Guide/scales.js
+++ src/components/Guide/scales.js
@@ -14,6 +14,7 @@ import DorianPic from "../../assets/Scales/dorian.png";
 import DorianSound from "../../assets/Scales/Dorian.mp3";
 import PhrygianPic from "../../assets/Scales/phrygian.png";
 import PhrygianSound from "../../assets/Scales/Phrygian.mp3";
+import VolumeUpIcon from "@material-ui/icons/VolumeUp";
 
 const playSound = (src) => {
   let sound = new Audio(src);
@@ -38,6 +39,83 @@ export default function IntervalGuide() {
         3rd. The interval between a C and E, however, is known as a major 3rd.
         This section will introduce all the intervals with audio examples.
       </h3>
+      <div className="guideStuff">
+        <div className="guideBox">
+          <div className="questionSpace">
+            <h2>Major</h2>
+            <button className="guideBtn" onClick={() => playSound(MajorSound)}>
+              <VolumeUpIcon />
+            </button>
+          </div>
+          <img src={MajorPic} className="scalePics" alt="" />
+        </div>
+        <div className="guideBox">
+          <div className="questionSpace">
+            <h2>Natural Minor</h2>
+            <button
+              className="guideBtn"
+              onClick={() => playSound(NatMinorSound)}
+            >
+              <VolumeUpIcon />
+            </button>
+          </div>
+          <img src={NatMinorPic} className="scalePics" alt="" />
+        </div>
+        <div className="guideBox">
+          <div className="questionSpace">
+            <h2>Harmonic Minor</h2>
+            <button
+              className="guideBtn"
+              onClick={() => playSound(HarmMinorSound)}
+            >
+              <VolumeUpIcon />
+            </button>
+          </div>
+          <img src={HarmMinorPic} className="scalePics" alt="" />
+        </div>
+        <div className="guideBox">
+          <div className="questionSpace">
+            <h2>Lydian</h2>
+            <button className="guideBtn" onClick={() => playSound(LydianSound)}>
+              <VolumeUpIcon />
+            </button>
+          </div>
+          <img src={LydianPic} className="scalePics" alt="" />
+        </div>
+        <div className="guideBox">
+          <div className="questionSpace">
+            <h2>Mixolydian</h2>
+            <button
+              className="guideBtn"
+              onClick={() => playSound(MixolydianSound)}
+            >
+              <VolumeUpIcon />
+            </button>
+          </div>
+          <img src={MixolydianPic} className="scalePics" alt="" />
+        </div>
+        <div className="guideBox">
+          <div className="questionSpace">
+            <h2>Dorian</h2>
+            <button className="guideBtn" onClick={() => playSound(DorianSound)}>
+              <VolumeUpIcon />
+            </button>
+          </div>
+          <img src={DorianPic} className="scalePics" alt="" />
+        </div>
+        <div className="guideBox">
+          <div className="questionSpace">
+            <h2>Phrygian</h2>
+            <button
+              className="guideBtn"
+              onClick={() => playSound(PhrygianSound)}
+            >
+              <VolumeUpIcon />
+            </button>
+          </div>
+          <img src={PhrygianPic} className="scalePics" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
