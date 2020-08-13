b50cd91794b44f476aa6bcef55173ae8c0ea8e96
Tejas Cousik <tcousik0@gmail.com>
2020-08-12 23:10:30 -0600

    adding to about

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./about.css";
 
 export default function about() {
   return (
-    <div>
+    <div className="container">
       <h2>Tejas Cousik</h2>
       <p>Info here</p>
     </div>
