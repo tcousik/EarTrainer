402ae171f1107d7ae2201e6999a773aba23bc557
Tejas Cousik <tcousik0@gmail.com>
2020-08-13 23:39:38 -0600

    filling out about

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,11 @@ export default function about() {
   return (
     <div className="container">
       <h2>Tejas Cousik</h2>
-      <p>Info here</p>
+      <img alt="headshot">Pic</img>
+      <h3>GitHub Link</h3>
+      <h3>LinkedIn Link</h3>
+      <h3>Phone and Email</h3>
+      <p>Bio</p>
     </div>
   );
 }
