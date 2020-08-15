d1ca02dcfebf957a43a4d2a91212ca24eb9b3fd3
Tejas Cousik <tcousik0@gmail.com>
2020-08-14 23:29:49 -0600

    cont

--- src/components/About.js
+++ src/components/About.js
@@ -4,19 +4,17 @@ import "./about.css";
 
 export default function about() {
   return (
-    <div
-      className="about
-    Container"
-    >
-      <div>
+    <div className="aboutContainer">
+      <div className="nameAndPic">
         <h2>Tejas Cousik</h2>
         <img id="pic" src={Pic} alt="headshot" />
       </div>
-      <div>
-        <h3>github.com/tcousik</h3>
-        <h3>LinkedIn Link</h3>
-        <h3>Phone and Email</h3>
-        <p>Bio</p>
+      <div className="contactInfo">
+        <a href="http://www.github.com/tcousik">Github</a>
+        <a href="http://www.linkedin.com/in/tcousik">LinkedIn</a>
+        <h3>github.com/tcousik LinkedIn Link</h3>
+        <h3>tcousik0@gmail.com (812)369-5600</h3>
+        <h3>Flatiron School</h3>
       </div>
     </div>
   );
