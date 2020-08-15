34d5e96315146767bdb4e2bfa33ff74ae9cc70a6
Tejas Cousik <tcousik0@gmail.com>
2020-08-14 23:16:28 -0600

    about cont.

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,23 @@
 import React from "react";
+import Pic from "../assets/headshot.jpg";
 import "./about.css";
 
 export default function about() {
   return (
-    <div className="container">
-      <h2>Tejas Cousik</h2>
-      <img alt="headshot">Pic</img>
-      <h3>GitHub Link</h3>
-      <h3>LinkedIn Link</h3>
-      <h3>Phone and Email</h3>
-      <p>Bio</p>
+    <div
+      className="about
+    Container"
+    >
+      <div>
+        <h2>Tejas Cousik</h2>
+        <img id="pic" src={Pic} alt="headshot" />
+      </div>
+      <div>
+        <h3>github.com/tcousik</h3>
+        <h3>LinkedIn Link</h3>
+        <h3>Phone and Email</h3>
+        <p>Bio</p>
+      </div>
     </div>
   );
 }
